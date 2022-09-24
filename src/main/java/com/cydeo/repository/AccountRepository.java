f00cd0a2334d3ed0b92776bb5d8412997efae760
saityagci <saityagci@gmail.com>
2022-09-24 02:55:17 -0400

    created AccountRepository,AccountService and AccountServiceImpl

--- src/main/java/com/cydeo/repository/AccountRepository.java
+++ src/main/java/com/cydeo/repository/AccountRepository.java
@@ -1,10 +1,11 @@
 package com.cydeo.repository;
 
 import com.cydeo.model.Account;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
-
+@Component
 public class AccountRepository {
     public static List<Account>accountList=new ArrayList<>();
     public Account save(Account account){
@@ -12,4 +13,7 @@ public class AccountRepository {
                 return account;
     }
 
+    public List<Account> findAll() {
+        return accountList;
+    }
 }
