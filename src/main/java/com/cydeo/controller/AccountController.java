71cc12f6562ef3f7933e75554ab6f5fafb4e5988
saityagci <saityagci@gmail.com>
2022-11-03 11:45:44 -0400

    return index html completed

--- src/main/java/com/cydeo/controller/AccountController.java
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -1,15 +1,21 @@
 package com.cydeo.controller;
 
+import com.cydeo.service.AccountService;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AccountController {
+    private final AccountService accountService;
+
+    public AccountController(AccountService accountService) {
+        this.accountService = accountService;
+    }
 
     @GetMapping("/index")
     public String getIndex(Model model){
-
+        model.addAttribute("accountList",accountService.listAllAccount());
         return "account/index";
     }
 }
