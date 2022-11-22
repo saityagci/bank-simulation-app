1c0fab64c40ef3bffdf6e3ff8921530bd8bd205a
saityagci <saityagci@gmail.com>
2022-11-21 19:30:18 -0500

    Account and transaction entity added

---
+++ src/main/java/com/cydeo/entity/Transaction.java
@@ -0,0 +1,33 @@
+package com.cydeo.entity;
+
+import com.cydeo.dto.AccountDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Account sender;
+    @ManyToOne
+    private Account receiver;
+    private BigDecimal amount;
+    private String message;
+    @Column(columnDefinition = "TIMESTAMP")
+    private Date creationDate;
+
+}
