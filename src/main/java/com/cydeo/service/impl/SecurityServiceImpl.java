532473b3dfa5382aa30d0de9a5e53a11c81318e5
saityagci <saityagci@gmail.com>
2023-01-26 17:34:05 -0500

    UserRepository and SecurityServiceImpl added

---
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.User;
+import com.cydeo.entity.common.UserPrincipal;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //we need to get our own user from database
+        User user=userRepository.findByUserName(username);
+        //return some exception if user does not exist
+        if (user==null){
+            throw new UsernameNotFoundException("This user does not exist");
+        }
+        //return user information in as a UserDetails
+        return new UserPrincipal(user);
+    }
+}
